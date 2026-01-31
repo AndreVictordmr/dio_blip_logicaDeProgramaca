bcd86fb04656e79d7784f6f3b8d81cc8fa4fc0bd
AndreVictorDmr <41845560+AndreVictordmr@users.noreply.github.com>
2026-01-31 17:21:08 -0300

    base

---
+++ desafios /desafio-classificador_de_nivel_heroi.js
@@ -0,0 +1,20 @@
+
+
+
+function classicacaoRankHeroi(xp){
+    if(xp<=1000){
+        console.log();
+    }else if(xp<=2000){
+
+    }else if (xp<=5000) {
+        
+    }else if (xp<=7000) {
+        
+    }else if (xp<=8000) {
+        
+    }else if (xp<=9000) {
+        
+    } else {
+        
+    }
+}
